e04ee5785c1c367f62e1e6f7f3b95b9ceff9da10
xz90 <xz90@illinois.edu>
2021-04-20 11:57:19 +0800

    Edition 3: finished basic animation of particles

--- src/my_simulator.cpp
+++ src/my_simulator.cpp
@@ -12,6 +12,7 @@
 
 using namespace ci;
 using namespace ci::app;
+using glm::vec2;
 
 namespace myapp {
 
@@ -31,10 +32,19 @@ namespace myapp {
 
     void MyApp::draw() {
         cinder::gl::clear();
+//        // red background
 //        cinder::gl::clear( cinder::Color( 1, 0, 0 ), true );
+
+//        // black white changing background
 //        float gray = float(sin( getElapsedSeconds() )) * 0.5f + 0.5f;
 //        cinder::gl::clear( cinder::Color( double(gray),double(gray),double(gray) ), true );
+
         gl::draw( myImage );
+        gl::drawSolidCircle( vec2( 800, 600 ), 50.0 );
+        double x = cos( getElapsedSeconds() )*50 + double(getWindowWidth())/2;
+        double y = sin( getElapsedSeconds() )*50 + double(getWindowHeight())/2;
+        gl::drawSolidCircle( vec2( x, y ), 50.0 );
+
 
     }
 
