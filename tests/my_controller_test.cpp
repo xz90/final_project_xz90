e5c42af8f33738f8ed38e95865a414ffb7745671
xz90 <xz90@illinois.edu>
2021-05-02 18:48:31 +0800

    Edition 3.1: Added test cases on particle controller

--- tests/my_controller_test.cpp
+++ tests/my_controller_test.cpp
@@ -1,4 +1,22 @@
 //
 // Created by Owenz on 4/19/2021.
 //
+#pragma warning( disable : 4819 )
+#include <catch2/catch.hpp>
+#include "../include/my_controller.h"
 
+using myapp::Particle;
+using namespace ci;
+using namespace myapp;
+using std::list;
+
+TEST_CASE("Controller constructor") {
+    SECTION("Default controller constructor") {
+        ParticleController controller;
+        SUCCEED();
+    }
+    SECTION("Add particles") {
+        ParticleController controller;
+        controller.addDefaultParticles(1);
+    }
+}
