65a771845b59c47c002a0c26348e52ced564c273
xz90 <xz90@illinois.edu>
2021-05-03 18:03:56 +0800

    Edition 3.6: finished my_particle2.h

--- include/my_particle2.h
+++ include/my_particle2.h
@@ -3,27 +3,29 @@
 //
 #pragma warning( disable : 4819 4305)
 #pragma once
+
 #ifndef FINAL_PROJECT_XZ90_MY_PARTICLE2_H
 #define FINAL_PROJECT_XZ90_MY_PARTICLE2_H
+#include "cinder\gl\gl.h"
+#include "cinder/Channel.h"
+#include "cinder/Vector.h"
+#include "cinder/Color.h"
+#include "cinder/Perlin.h"
+#include <vector>
 
+using glm::vec2;
+namespace myapp{
+    class Particle2 {
+        Particle( vec2 );
+        void update();
+        void draw();
+        vec2 mLoc;
+        vec2 mVel;
+        float mRadius;
+    };
+} // namespace myapp
 
-class my_particle2 {
-
-};
 
 
 #endif //FINAL_PROJECT_XZ90_MY_PARTICLE2_H
 
-
-#pragma once
-#include "Particle.h"
-#include <list>
-class ParticleController {
-public:
-    ParticleController();
-    void update();
-    void draw();
-    void addParticles( int amt );
-    void removeParticles( int amt );
-    std::list<Particle> mParticles;
-};
